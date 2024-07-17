95213c9f444acd3c9270bfe86ad864f2d2958064
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2024-07-17 23:51:27 +0700

    Add container component.

---
+++ src/container/container.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class='cm-container'>
+        <slot />
+    </div>
+</template>
+
+<script setup lang='ts'>
+import('./container.scss');
+</script>
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { default as CmLogo } from './logo/logo.vue';
 export { default as CmHeader } from './header/header.vue';
+export { default as CmContainer } from './container/container.vue';
