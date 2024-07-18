50fe78d532ce046fbfa8fdb1a123d3ec7c0cc7fd
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2024-07-18 23:23:19 +0700

    Added button component.

---
+++ src/button/button.vue
@@ -0,0 +1,25 @@
+<template>
+    <button class='cm-button' :class='`cm-button_${variant}`'>
+        <template v-if='$slots.default'>
+            <slot />
+        </template>
+        <template v-else>
+            {{ label }}
+        </template>
+    </button>
+</template>
+
+<script setup lang='ts'>
+import('./button.scss');
+import { defineProps } from 'vue';
+
+interface Props {
+    label?: string,
+    variant: string,
+}
+
+withDefaults(defineProps<Props>(), {
+    label: '',
+    variant: 'primary',
+});
+</script>
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as CmLogo } from './logo/logo.vue';
 export { default as CmHeader } from './header/header.vue';
+export { default as CmButton } from './button/button.vue';
 export { default as CmContainer } from './container/container.vue';
