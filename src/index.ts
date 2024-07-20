04c69c4d93a4d8ec91f672150f9587e167c54502
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2024-07-21 01:10:33 +0700

    Added menu component.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
-export { default as CmLogo } from './logo/logo.vue';
-export { default as CmHeader } from './header/header.vue';
-export { default as CmButton } from './button/button.vue';
-export { default as CmContainer } from './container/container.vue';
+import { default as CmLogo } from './logo/logo.vue';
+import { default as CmMenu } from './menu/menu.vue';
+import { default as CmHeader } from './header/header.vue';
+import { default as CmButton } from './button/button.vue';
+import { default as CmContainer } from './container/container.vue';
+
+export {
+    CmLogo,
+    CmMenu,
+    CmHeader,
+    CmButton,
+    CmContainer,
+};
---
+++ src/menu/menu.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class='cm-menu' :class='`cm-menu_${orientation}`'>
+        <ul class='cm-menu__list'>
+            <li v-for='(item, index) in items' :key='getKey(item, index)' class='cm-menu__item'>
+                <slot
+                    v-if='$slots[getKey(item, index)]'
+                    :name='getKey(item, index)'
+                    :item='{ ...item, ...{class: "cm-menu__link"} }'
+                />
+                <a v-else class='cm-menu__link' :href='item.url'>{{ item.label }}</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup lang='ts'>
+import('./menu.scss');
+import { defineProps } from 'vue';
+
+interface Item {
+    label: string,
+    url: string,
+}
+
+interface Props {
+    items: Array<Item>,
+    orientation?: string,
+}
+
+withDefaults(defineProps<Props>(), {
+    orientation: 'vertical',
+});
+
+function getKey(item: Item, index: number) {
+    return `${item.label}_${index.toString()}`;
+}
+</script>
