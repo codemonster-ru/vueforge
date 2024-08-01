489fd44435e0601068bb990991f02a21aad80382
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2024-08-01 20:45:43 +0700

    Renamed vite.config.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import { copyFileSync } from 'node:fs';
+import { fileURLToPath, URL } from 'url';
 import vue from '@vitejs/plugin-vue';
 import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+defineConfig({
     plugins: [
         vue(),
         dts({
@@ -14,6 +15,11 @@ export default defineConfig({
             },
         }),
     ],
+    resolve: {
+        alias: [
+            { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
+        ],
+    },
     build: {
         cssCodeSplit: true,
         lib: {
