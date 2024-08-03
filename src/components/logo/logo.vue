90faf8da02b6e6fb6c58df4cadd7fb50dc8baaf4
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2024-08-03 17:09:56 +0700

    Added support switching light and dark logo and using link.

--- src/components/logo/logo.vue
+++ src/components/logo/logo.vue
@@ -1,16 +1,48 @@
 <template>
     <div class='cm-logo'>
-        <img :src='src' :width='width' :height='height' :alt='alt'>
+        <template v-if='type.length'>
+            <cm-link :type='type' :to='to' :url='url'>
+                <img :src='getSrc' :width='width' :height='height' :alt='alt'>
+            </cm-link>
+        </template>
+        <template v-else>
+            <img :src='getSrc' :width='width' :height='height' :alt='alt'>
+        </template>
     </div>
 </template>
 
 <script setup lang='ts'>
-import('./logo.scss');
+import { RouteLocationAsPathGeneric, RouteLocationAsRelativeGeneric } from 'vue-router';
 
-defineProps<{
-    src: string,
+import('./logo.scss');
+import { CmLink } from '@/index';
+import { computed, defineProps } from 'vue';
+
+interface Props {
+    to?: string | RouteLocationAsRelativeGeneric | RouteLocationAsPathGeneric,
+    url?: string,
+    src: string | Array<string>,
     alt?: string,
+    type?: string,
+    dark?: boolean,
     width?: string,
     height?: string,
-}>();
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    to: undefined,
+    alt: undefined,
+    url: undefined,
+    type: '',
+    width: undefined,
+    height: undefined,
+});
+
+const getSrc = computed(() => {
+    if (Array.isArray(props.src)) {
+        return props.dark ? props.src[1] : props.src[0];
+    } else {
+        return props.src;
+    }
+});
 </script>
