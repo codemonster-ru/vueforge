0b2b981091b35a7aa6ddbb993d889aa05be45a13
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2024-10-10 05:38:22 +0700

    Added card component.

---
+++ src/lib/components/card/card.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class='cm-card'>
+        <div v-if='$slots.default' class='cm-card__default'>
+            <slot name='default' />
+        </div>
+        <div v-if='$slots.header' class='cm-card__header'>
+            <slot name='header' />
+        </div>
+        <div v-if='$slots.body' class='cm-card__body'>
+            <slot name='body' />
+        </div>
+        <div v-if='$slots.footer' class='cm-card__footer'>
+            <slot name='footer' />
+        </div>
+    </div>
+</template>
+
+<script setup lang='ts'>
+import('./card.scss');
+import('./cardTheme.scss');
+</script>
