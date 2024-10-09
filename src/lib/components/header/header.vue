e32211ef702e6c7b4f8f0c183b55f4323f52a85b
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2024-10-10 05:37:23 +0700

    Changed header component.

--- src/lib/components/header/header.vue
+++ src/lib/components/header/header.vue
@@ -1,10 +1,19 @@
 <template>
     <header class='cm-header'>
-        <slot />
+        <cm-container>
+            <div v-if='$slots.left' class='cm-header__left'>
+                <slot name='left' />
+            </div>
+            <div v-if='$slots.right' class='cm-header__right'>
+                <slot name='right' />
+            </div>
+        </cm-container>
     </header>
 </template>
 
 <script setup lang='ts'>
 import('./header.scss');
 import('./headerTheme.scss');
+
+import { CmContainer } from '@/lib';
 </script>
