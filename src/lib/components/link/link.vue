2e88697244d7ec067e581d7e7d752461b8382c42
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2024-10-10 05:36:57 +0700

    Changed link component.

--- src/lib/components/link/link.vue
+++ src/lib/components/link/link.vue
@@ -1,5 +1,10 @@
 <template>
-    <a v-if='type === "a"' :href='url' class='cm-link'>
+    <a
+        v-if='type === "a"'
+        :href='url'
+        class='cm-link'
+        :class='{ "cm-link_active": active, "cm-link_disabled": disabled }'
+    >
         <template v-if='$slots.default'>
             <slot />
         </template>
@@ -7,7 +12,16 @@
             {{ label }}
         </template>
     </a>
-    <router-link v-else :to='to' class='cm-link'>
+    <router-link
+        v-else
+        ref='link'
+        :to='to'
+        class='cm-link'
+        :class='{ "cm-link_active": getActive }'
+        :disabled='disabled'
+        active-class='cm-link_partially-active'
+        exact-active-class='cm-link_active'
+    >
         <template v-if='$slots.default'>
             <slot />
         </template>
@@ -21,20 +35,34 @@
 import('./link.scss');
 import('./linkTheme.scss');
 
-import { RouteLocationAsRelativeGeneric, RouteLocationAsPathGeneric } from 'vue-router';
-import { defineProps } from 'vue';
+import { useRoute, RouteLocationAsRelativeGeneric, RouteLocationAsPathGeneric } from 'vue-router';
+import { computed, defineProps, ref } from 'vue';
 
 interface Props {
     to?: string | RouteLocationAsRelativeGeneric | RouteLocationAsPathGeneric,
     url?: string,
     type: string,
     label?: string,
+    active?: boolean,
+    disabled?: boolean,
 }
 
-withDefaults(defineProps<Props>(), {
+const emits = defineEmits(['onActive']);
+const props = withDefaults(defineProps<Props>(), {
     to: '',
     url: undefined,
     type: 'a',
     label: '',
+});
+const route = useRoute();
+const link = ref(null);
+const getActive = computed(() => {
+    if (props.type === 'router-link') {
+        if (route.matched.some(({ name }) => props.to.name === name)) {
+            emits('onActive');
+        }
+    }
+
+    return props.active;
 });
 </script>
