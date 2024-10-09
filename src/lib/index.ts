92b6e190fc46e226655c759b9d285299f66880c1
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2024-10-10 05:46:31 +0700

    Added new component export.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,19 +1,33 @@
+import CmBody from '@/lib/components/body/body.vue';
 import CmLink from '@/lib/components/link/link.vue';
+import CmCard from '@/lib/components/card/card.vue';
 import CmLogo from '@/lib/components/logo/logo.vue';
 import CmMenu from '@/lib/components/menu/menu.vue';
 import CmHeader from '@/lib/components/header/header.vue';
 import CmFooter from '@/lib/components/footer/footer.vue';
 import CmButton from '@/lib/components/button/button.vue';
+import CmHeading from '@/lib/components/heading/heading.vue';
 import CmContent from '@/lib/components/content/content.vue';
+import CmPopover from '@/lib/components/popover/popover.vue';
 import CmContainer from '@/lib/components/container/container.vue';
+import CmDefaultLayout from '@/lib/layouts/defaultLayout.vue';
+import vCmClickOutside from '@/lib/directives/clickOutside';
+import CmLeftSidebarLayout from '@/lib/layouts/leftSidebarLayout.vue';
 
 export {
+    CmBody,
     CmLink,
+    CmCard,
     CmLogo,
     CmMenu,
     CmFooter,
     CmHeader,
     CmButton,
+    CmHeading,
     CmContent,
+    CmPopover,
     CmContainer,
+    CmDefaultLayout,
+    vCmClickOutside,
+    CmLeftSidebarLayout,
 };
