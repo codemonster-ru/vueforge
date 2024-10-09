91e923198da1bf267ab9fe6bddd60284830b9580
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2024-10-10 05:34:26 +0700

    Added click outside directive.

---
+++ src/lib/directives/clickOutside.ts
@@ -0,0 +1,21 @@
+export default {
+    mounted: function(element, binding) {
+        element.clickOutsideEvent = function(event) {
+            const excludeComponent = document.getElementById(binding.arg);
+
+            if (
+                !(element == event.target || element.contains(event.target)) &&
+                !(
+                    excludeComponent &&
+                    (event.target == excludeComponent || excludeComponent.contains(event.target))
+                )
+            ) {
+                binding.value(event, element);
+            }
+        };
+        document.addEventListener('click', element.clickOutsideEvent);
+    },
+    unmounted: function(element) {
+        document.removeEventListener('click', element.clickOutsideEvent);
+    },
+};
