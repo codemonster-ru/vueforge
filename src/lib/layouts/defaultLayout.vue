3788b3cd67209de308540fef16382357d0d14ae8
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2024-10-10 05:39:11 +0700

    Added default layout.

---
+++ src/lib/layouts/defaultLayout.vue
@@ -0,0 +1,24 @@
+<template>
+    <cm-header>
+        <template #left>
+            <slot name='headerLeft' />
+        </template>
+        <template #right>
+            <slot name='headerRight' />
+        </template>
+    </cm-header>
+    <cm-content>
+        <cm-container>
+            <router-view />
+        </cm-container>
+    </cm-content>
+    <cm-footer>
+        <cm-container>
+            <slot name='footerDefault' />
+        </cm-container>
+    </cm-footer>
+</template>
+
+<script setup lang='ts'>
+import { CmHeader, CmContent, CmFooter, CmContainer } from '..';
+</script>
