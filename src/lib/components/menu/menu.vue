9ed5c5a8477325e1ea6612edfd7d6e440d71abdd
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2024-10-10 05:36:26 +0700

    Changed menu component.

--- src/lib/components/menu/menu.vue
+++ src/lib/components/menu/menu.vue
@@ -7,7 +7,37 @@
                     :name='getKey(item, index)'
                     :item='{ ...item, ...{class: "cm-menu__link"} }'
                 />
-                <cm-link v-else :type='getType(item)' :to='item.to' :url='item.url' class='cm-menu__link'>
+                <hr v-else-if='item.separator' class='cm-menu__separator' />
+                <template v-else-if='item.hasOwnProperty("items")'>
+                    <div
+                        class='cm-menu__parent'
+                        :class='{ "cm-menu__parent_active": item.active }'
+                        @click='onClick(item)'
+                    >
+                        <cm-icon v-if='item.icon' :icon='item.icon' class='cm-menu__icon' />
+                        {{ item.label }}
+                        <cm-icon :icon='CmChevronDownIcon' />
+                    </div>
+                    <cm-menu
+                        v-if='item.items'
+                        :items='item.items'
+                        class='cm-menu__submenu'
+                        :class='{ "cm-menu__submenu_visible": item.subMenuVisible }'
+                        @on-active='onActive'
+                    />
+                </template>
+                <cm-link
+                    v-else
+                    :to='item.to'
+                    :url='item.url'
+                    :type='getType(item)'
+                    class='cm-menu__link'
+                    :active='item.active'
+                    :disabled='item.disabled'
+                    @click='onClick(item)'
+                    @on-active='onActive(item)'
+                >
+                    <cm-icon v-if='item.icon' :icon='item.icon' class='cm-menu__icon'></cm-icon>
                     {{ item.label }}
                 </cm-link>
             </li>
@@ -18,13 +48,20 @@
 <script setup lang='ts'>
 import('./menu.scss');
 import('./menuTheme.scss');
-import { CmLink } from '@/lib';
-import { defineProps } from 'vue';
+import { CmIcon, CmChevronDownIcon } from '@codemonster-ru/icons';
+import { CmMenu, CmLink } from '@/lib';
+import { Component, defineProps } from 'vue';
 
 interface Item {
-    label: string,
     to?: string,
     url?: string,
+    icon?: Component,
+    items?: Array<Item>
+    label?: string,
+    active?: boolean,
+    disabled?: boolean,
+    separator?: boolean,
+    subMenuVisible?: boolean
 }
 
 interface Props {
@@ -32,15 +69,37 @@ interface Props {
     orientation?: string,
 }
 
-withDefaults(defineProps<Props>(), {
-    orientation: 'vertical',
-});
+const emits = defineEmits(['onActive']);
+const props = withDefaults(defineProps<Props>(), { orientation: 'vertical' });
+const getKey = (item: Item, index: number) => `${item.label}_${index.toString()}`;
+const getType = (item: Item) => Object.prototype.hasOwnProperty.call(item, 'to') ? 'router-link' : 'a';
+const onClick = (item) => {
+    if (Object.prototype.hasOwnProperty.call(item, 'items')) {
+        props.items.map((x) => {
+            if (x !== item) {
+                x.subMenuVisible = false;
+            }
+        });
+        item.subMenuVisible = !item.subMenuVisible;
+    } else if (Object.prototype.hasOwnProperty.call(item, 'command')) {
+        item.command();
+    }
+};
+const onActive = (item) => {
+    for (const index in props.items) {
+        const loopItem = props.items[index];
 
-function getKey(item: Item, index: number) {
-    return `${item.label}_${index.toString()}`;
-}
+        if (Object.prototype.hasOwnProperty.call(loopItem, 'items')) {
+            if (loopItem.items?.some((x) => x === item)) {
+                loopItem.active = true;
+                loopItem.subMenuVisible = true;
+            } else {
+                loopItem.active = false;
+                loopItem.subMenuVisible = false;
+            }
+        }
+    }
 
-function getType(item: Item) {
-    return Object.prototype.hasOwnProperty.call(item, 'to') ? 'router-link' : 'a';
-}
+    emits('onActive', item);
+};
 </script>
