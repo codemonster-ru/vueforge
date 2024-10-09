2bb1c6523128db18fea7e03bc170b4984ffd44ab
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2024-10-10 05:39:42 +0700

    Added left sidebar layout.

---
+++ src/lib/layouts/leftSidebarLayout.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class='cm-left-sidebar-flexbox'>
+        <div class='cm-left-sidebar'>
+            <slot name='leftSidebar' />
+        </div>
+        <div class='cm-main-content'>
+            <router-view />
+        </div>
+    </div>
+</template>
+
+<style lang='scss'>
+.cm-left-sidebar-flexbox {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+}
+
+.cm-left-sidebar {
+    width: 250px;
+    margin-right: 48px;
+}
+
+.cm-main-content {
+    width: calc(100% - (250px + 48px));
+}
+</style>
