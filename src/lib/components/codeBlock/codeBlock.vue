c0e5a541ea4d0f86e5dddfa00695ca0c2107a4b1
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2024-11-12 00:46:44 +0700

    Added codeBlock component.

---
+++ src/lib/components/codeBlock/codeBlock.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class='cm-code-block'>
+        <pre class='cm-code-block__pre'>
+            <code class='cm-code-block__code'>
+                <slot />
+            </code>
+        </pre>
+    </div>
+</template>
+
+<script setup lang='ts'>
+</script>
+
+<style lang='scss'>
+.cm-code-block {
+    display: flex;
+    margin-bottom: 12px;
+    border-radius: 6px;
+    justify-content: space-between;
+    background-color: var(--cm-bg-soft);
+}
+
+.cm-code-block__pre {
+    flex: 1;
+    margin: 0;
+    display: flex;
+    padding: 24px;
+    overflow: auto;
+    line-height: 1.45;
+}
+
+.cm-code-block__code {
+    display: flex;
+    flex-direction: column;
+}
+</style>
