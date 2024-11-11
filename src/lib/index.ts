5a1700743303631e02588687a5c688a5e5bf534f
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2024-11-12 00:50:23 +0700

    Added demo, codeBlock components to export.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,4 @@
+import CmDemo from '@/lib/components/demo/demo.vue';
 import CmBody from '@/lib/components/body/body.vue';
 import CmLink from '@/lib/components/link/link.vue';
 import CmCard from '@/lib/components/card/card.vue';
@@ -6,15 +7,16 @@ import CmMenu from '@/lib/components/menu/menu.vue';
 import CmHeader from '@/lib/components/header/header.vue';
 import CmFooter from '@/lib/components/footer/footer.vue';
 import CmButton from '@/lib/components/button/button.vue';
-import CmHeading from '@/lib/components/heading/heading.vue';
 import CmContent from '@/lib/components/content/content.vue';
 import CmPopover from '@/lib/components/popover/popover.vue';
 import CmContainer from '@/lib/components/container/container.vue';
+import CmCodeBlock from '@/lib/components/codeBlock/codeBlock.vue';
 import CmDefaultLayout from '@/lib/layouts/defaultLayout.vue';
 import vCmClickOutside from '@/lib/directives/clickOutside';
 import CmLeftSidebarLayout from '@/lib/layouts/leftSidebarLayout.vue';
 
 export {
+    CmDemo,
     CmBody,
     CmLink,
     CmCard,
@@ -23,9 +25,9 @@ export {
     CmFooter,
     CmHeader,
     CmButton,
-    CmHeading,
     CmContent,
     CmPopover,
+    CmCodeBlock,
     CmContainer,
     CmDefaultLayout,
     vCmClickOutside,
