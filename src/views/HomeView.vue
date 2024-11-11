d7b8ad95feee513e8be60cf7dfd6053e21438f3e
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2024-11-12 00:54:19 +0700

    Changed home view in docs app.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,1 +1,32 @@
-<template>Hello World!</template>
+<template>
+    <div class='welcome'>
+        <h1 class='welcome__heading'>UI component suite for Vue.js</h1>
+        <p class='welcome__description'>Enhance your web applications with comprehensive suite of customizable,
+            feature-rich UI components.</p>
+        <cm-button :to='{ name: "docs" }' type='router-link'>Get started</cm-button>
+    </div>
+</template>
+
+<script setup lang='ts'>
+import { CmButton } from '@/lib';
+</script>
+
+<style lang='scss'>
+.welcome {
+    width: 100%;
+    padding: 70px 0;
+    text-align: center;
+}
+
+.welcome__heading {
+    font-size: 36px;
+    line-height: 44px;
+    margin-bottom: 12px;
+}
+
+.welcome__description {
+    color: var(--cm-text-2);
+    font-size: 18px;
+    margin-bottom: 28px;
+}
+</style>
