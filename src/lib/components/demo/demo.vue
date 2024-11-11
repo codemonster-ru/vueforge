753364946bce48fddd2c8d9db6e88d6cf703d84b
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2024-11-12 00:46:58 +0700

    Added demo component.

---
+++ src/lib/components/demo/demo.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class='cm-demo'>
+        <slot />
+    </div>
+</template>
+
+<style lang='scss'>
+.cm-demo {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 24px;
+    border-radius: 6px;
+    margin-bottom: 12px;
+    background-color: var(--cm-bg-soft);
+}
+</style>
