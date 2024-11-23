4b5dfe1b46b350d11f66d12079f134843e9e5068
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2024-11-23 22:48:19 +0700

    Changed export in index app to one line.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,35 +1,15 @@
-import CmDemo from '@/lib/components/demo.vue';
-import CmBody from '@/lib/components/body.vue';
-import CmLink from '@/lib/components/link.vue';
-import CmCard from '@/lib/components/card.vue';
-import CmLogo from '@/lib/components/logo.vue';
-import CmMenu from '@/lib/components/menu.vue';
-import CmHeader from '@/lib/components/header.vue';
-import CmFooter from '@/lib/components/footer.vue';
-import CmButton from '@/lib/components/button.vue';
-import CmContent from '@/lib/components/content.vue';
-import CmPopover from '@/lib/components/popover.vue';
-import CmContainer from '@/lib/components/container.vue';
-import CmCodeBlock from '@/lib/components/codeBlock.vue';
-import CmDefaultLayout from '@/lib/layouts/defaultLayout.vue';
-import vCmClickOutside from '@/lib/directives/clickOutside';
-import CmLeftSidebarLayout from '@/lib/layouts/leftSidebarLayout.vue';
-
-export {
-    CmDemo,
-    CmBody,
-    CmLink,
-    CmCard,
-    CmLogo,
-    CmMenu,
-    CmFooter,
-    CmHeader,
-    CmButton,
-    CmContent,
-    CmPopover,
-    CmCodeBlock,
-    CmContainer,
-    CmDefaultLayout,
-    vCmClickOutside,
-    CmLeftSidebarLayout,
-};
+export { default as CmDemo } from '@/lib/components/demo.vue';
+export { default as CmLink } from '@/lib/components/link.vue';
+export { default as CmCard } from '@/lib/components/card.vue';
+export { default as CmLogo } from '@/lib/components/logo.vue';
+export { default as CmMenu } from '@/lib/components/menu.vue';
+export { default as CmHeader } from '@/lib/components/header.vue';
+export { default as CmFooter } from '@/lib/components/footer.vue';
+export { default as CmButton } from '@/lib/components/button.vue';
+export { default as CmContent } from '@/lib/components/content.vue';
+export { default as CmPopover } from '@/lib/components/popover.vue';
+export { default as CmContainer } from '@/lib/components/container.vue';
+export { default as CmCodeBlock } from '@/lib/components/codeBlock.vue';
+export { default as CmDefaultLayout } from '@/lib/layouts/defaultLayout.vue';
+export { default as vCmClickOutside } from '@/lib/directives/clickOutside';
+export { default as CmLeftSidebarLayout } from '@/lib/layouts/leftSidebarLayout.vue';
