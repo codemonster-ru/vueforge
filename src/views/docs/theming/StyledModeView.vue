c986511678c6574d03a5ae54287675e72b89163a
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2024-12-15 22:21:55 +0700

    Added info about styled mode in docs.

--- src/views/docs/theming/StyledModeView.vue
+++ src/views/docs/theming/StyledModeView.vue
@@ -4,4 +4,19 @@
     <h2>Configuration API</h2>
     <h3>Theme</h3>
     <p>The theme property is used to customize the initial theme.</p>
+    <CodeBlock>
+        <span>import { Config, DefaultTheme } from './lib';</span>
+        <br>
+        <span>const app = createApp(App);</span>
+        <br>
+        <span>app.use(Config, {</span>
+        <span>&nbsp;&nbsp;&nbsp;&nbsp;theme: {</span>
+        <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;preset: DefaultTheme,</span>
+        <span>&nbsp;&nbsp;&nbsp;&nbsp;},</span>
+        <span>});</span>
+    </CodeBlock>
 </template>
+
+<script setup lang='ts'>
+import { CodeBlock } from '@/lib';
+</script>
