09e6a07a23cac674d7973f010fc7e81eb47c301e
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-05-05 22:29:01 +0700

    Set height to 100vh in default layout.

--- src/layouts/defaultLayout.vue
+++ src/layouts/defaultLayout.vue
@@ -1,24 +1,34 @@
 <template>
-    <Header>
-        <template #left>
-            <slot name='headerLeft' />
-        </template>
-        <template #right>
-            <slot name='headerRight' />
-        </template>
-    </Header>
-    <Content>
-        <Container>
-            <router-view />
-        </Container>
-    </Content>
-    <Footer>
-        <Container>
-            <slot name='footerDefault' />
-        </Container>
-    </Footer>
+    <div class="vf-layout">
+        <Header>
+            <template #left>
+                <slot name="headerLeft" />
+            </template>
+            <template #right>
+                <slot name="headerRight" />
+            </template>
+        </Header>
+        <Content>
+            <Container>
+                <router-view />
+            </Container>
+        </Content>
+        <Footer>
+            <Container>
+                <slot name="footerDefault" />
+            </Container>
+        </Footer>
+    </div>
 </template>
 
-<script setup lang='ts'>
-import { Header, Content, Footer, Container } from '..';
+<script setup lang="ts">
+import { Header, Content, Footer, Container } from '@/index';
 </script>
+
+<style>
+.vf-layout {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+}
+</style>
