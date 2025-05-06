3d51f6cf7166d8868e442a817027459bae19adbd
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-05-06 21:08:16 +0700

    Added padding in main page of example.

--- src/example/views/HomeView.vue
+++ src/example/views/HomeView.vue
@@ -1,9 +1,18 @@
 <template>
-    <h1>Examples</h1>
-    <h2>Layouts</h2>
-    <ul>
-        <li>
-            <router-link to="/layouts/default">Default</router-link>
-        </li>
-    </ul>
+    <div class="vf-home">
+        <h1>Examples</h1>
+        <h2>Layouts</h2>
+        <ul>
+            <li>
+                <router-link to="/layouts/default">Default</router-link>
+            </li>
+        </ul>
+    </div>
 </template>
+
+<style>
+.vf-home {
+    padding-right: 1rem;
+    padding-left: 1rem;
+}
+</style>
