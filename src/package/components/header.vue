30af13fa347865d81c9f00f42254f4cd1ccc2ec1
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-05-11 19:32:54 +0700

    Changed styles in the header component.

--- src/package/components/header.vue
+++ src/package/components/header.vue
@@ -4,14 +4,13 @@
     </header>
 </template>
 
-<style lang="scss">
+<style>
 .vf-header {
     height: var(--vf-header-height);
-    min-height: var(--vf-header-min-height);
     display: flex;
     padding-top: var(--vf-header-padding);
-    border-bottom: 1px solid var(--vf-header-border-color);
-    margin-bottom: 24px;
+    border-bottom: var(--vf-header-border-bottom);
+    margin-bottom: var(--vf-header-margin-bottom);
     padding-bottom: var(--vf-header-padding);
     background-color: var(--vf-header-background-color);
 }
--- src/package/themes/default/components/header.ts
+++ src/package/themes/default/components/header.ts
@@ -1,7 +1,8 @@
 export default {
-    height: '60px',
-    padding: '12px',
-    minHeight: '60px',
+    height: '55px',
+    padding: '0',
     borderColor: 'var(--vf-border-color)',
+    borderBottom: '1px solid var(--vf-header-border-color)',
+    marginBottom: '24px',
     backgroundColor: 'var(--vf-bg-color)',
 };
